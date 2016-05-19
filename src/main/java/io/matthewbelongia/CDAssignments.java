6e3420783640905fe6b0f384b16c538443d33b27
Matthew Belongia <matthewb@Zipcoder18s-MacBook-Pro.local>
2016-05-19 09:31:52 -0400

    all cds

--- src/main/java/io/matthewbelongia/CDAssignments.java
+++ src/main/java/io/matthewbelongia/CDAssignments.java
@@ -1,11 +1,12 @@
 package io.matthewbelongia;
+
 import java.util.*;
 //import static net.mindview.util.Print.*; didnt download library
 
 /**
  * Created by matthewb on 4/27/16.
  */
-public class TestingJava {
+public class CDAssignments {
     public static void main(String[] args) {
         //System.out.println("Today's date is:");
         //System.out.println(new Date());
@@ -25,14 +26,15 @@ public class TestingJava {
 
     }
 
-    public static void disp(String var1){
+    public static void disp(String var1) {
         System.out.println("One String " + var1);
     }
-    public static void disp(String var1, String var2){
+
+    public static void disp(String var1, String var2) {
         System.out.println("Two Strings " + var1 + " " + var2);
     }
 
-    public static void vampireNumber(){
+    public static void vampireNumber() {
         String stringx = "";
         String stringy = "";
         String bothStrings = "";
@@ -40,13 +42,13 @@ public class TestingJava {
         int bothXandY = 0;
 
         ArrayList<Integer> vampNums = new ArrayList<Integer>();
-        for(int x = 10;x<100;x++){
-            for(int y = 10;y<100;y++){
-                if(x * y > 1000){
+        for (int x = 10; x < 100; x++) {
+            for (int y = 10; y < 100; y++) {
+                if (x * y > 1000) {
                     bothXandY = x * y;
                     stringx = Integer.toString(x);
                     stringy = Integer.toString(y);
-                    bothStrings = stringx+stringy;
+                    bothStrings = stringx + stringy;
                     char[] chars = bothStrings.toCharArray();
                     Arrays.sort(chars);
                     String sorted = new String(chars);
@@ -54,7 +56,7 @@ public class TestingJava {
                     char[] otherchars = combined.toCharArray();
                     Arrays.sort(otherchars);
                     String othersorted = new String(otherchars);
-                    if(sorted.equals(othersorted) && !vampNums.contains(bothXandY)){
+                    if (sorted.equals(othersorted) && !vampNums.contains(bothXandY)) {
                         vampNums.add(bothXandY);
                     }
                 }
@@ -65,44 +67,43 @@ public class TestingJava {
 
     }
 
-    public static void javaFiboDisplayAll(int display){
+    public static void javaFiboDisplayAll(int display) {
         String result = "";
-        for(int i = 0; i < display;i++){
+        for (int i = 0; i < display; i++) {
 
             result += " " + javaFibo(i);
         }
         System.out.println(result);
     }
 
-    public static int javaFibo(int howmany){
+    public static int javaFibo(int howmany) {
         int total = 0;
-        if(howmany == 0) {
+        if (howmany == 0) {
             //System.out.println("0");
             return 0;
         }
-        if(howmany == 1) {
+        if (howmany == 1) {
             //System.out.println("1");
             return 1;
-        }
-        else
-            return  javaFibo(howmany - 1)+ javaFibo(howmany-2);
+        } else
+            return javaFibo(howmany - 1) + javaFibo(howmany - 2);
     }
 
-    public static void CommaOperator(){
-        for(int i = 1, j = i+ 10; i < 5; i++, j = i*2){
+    public static void CommaOperator() {
+        for (int i = 1, j = i + 10; i < 5; i++, j = i * 2) {
             System.out.println("i = " + i + " j = " + j);
         }
     }
-    public static void Random25Ints(){
+
+    public static void Random25Ints() {
         Random rand = new Random();
         int compareMe = rand.nextInt();
         System.out.println(compareMe);
-        for(int x =0;x<25;x++){
+        for (int x = 0; x < 25; x++) {
             int rando = rand.nextInt();
-            if(rando > compareMe) {
+            if (rando > compareMe) {
                 System.out.println("rando is bigger than compareMe");
-            }
-            else{
+            } else {
                 System.out.println("rando is tiny");
             }
 
@@ -110,11 +111,11 @@ public class TestingJava {
     }
 
     public static void StringCompare(String string1, String string2) {
-        if(string1 == string2)
+        if (string1 == string2)
             System.out.println("They equal each other");
-        if(string1.equals(string2))
+        if (string1.equals(string2))
             System.out.println("Contents equal each other");
-        if(string1 != string2)
+        if (string1 != string2)
             System.out.println("Not equal");
 
     }
@@ -128,13 +129,11 @@ public class TestingJava {
         String input = name.next();
         String personName = input;
 
-        if(personName.equals("Alice")||personName.equals("Bob"))
+        if (personName.equals("Alice") || personName.equals("Bob"))
 
         {
             System.out.println("Hello " + personName);
-        }
-
-        else
+        } else
 
         {
             System.out.println("I don't know you!");
@@ -142,7 +141,7 @@ public class TestingJava {
 
     }
 
-    private static void flipcoin(int flips){
+    private static void flipcoin(int flips) {
         Random rand = new Random();
 
         for (int x = flips; x < 10; x++) {
@@ -153,9 +152,16 @@ public class TestingJava {
             else
                 System.out.println("Tails");
         }
-        }
-
-
-
     }
+
+    public void countToN(){
+        Scanner numberIntake = new Scanner(System.in);
+        int number = numberIntake.nextInt();
+        for(int q =0;q<number;q++){
+            System.out.println(q);
+        }
+    }
+
+
+}
 
